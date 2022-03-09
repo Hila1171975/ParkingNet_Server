9df37f9949cd4c4f84b0290f2943468665af3f6c
Hila <hila1171975@gmail.com>
2022-03-09 18:54:35 +0200

    סתם קבצים שחייב להעלות כדי להסתנכרן

---
+++ Parking_NET_Project/Controllers/FindParkingController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BL;
+using Entities;
+
+namespace Parking_NET_Project.Controllers
+{
+    [RoutePrefix("api/FindParking")]
+    public class FindParkingController : ApiController
+    {
+
+        // חיפוש 3 חניות הקרובות ביותר
+        [HttpPost]
+        [Route("Search3Parkings/{home_lat}/{home_lan}")]
+        public IHttpActionResult Search3Parkings(double home_lat, double home_lan, [FromBody] RentEntities r)
+        {
+            try
+            {
+                return Ok(FindParkingBl.start(home_lat, home_lan, r));
+            }
+            catch
+            {
+                return Ok(true);
+            }
+        }
+    }
+}
