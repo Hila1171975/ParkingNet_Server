518fe51463f26725d529eadb94dfbbd7c46b6f40
Hila <hila1171975@gmail.com>
2022-03-23 21:44:00 +0200

    statics list

--- BL/FindParkingBl.cs
+++ BL/FindParkingBl.cs
@@ -1,6 +1,6 @@
 ﻿using DAL;
 using Entities;
-using GoogleApi.Entities.Common;
+using GoogleApi.Entities.Maps.Common;
 using GoogleApi.Entities.Maps.Directions.Request;
 using GoogleApi.Entities.Maps.Directions.Response;
 
@@ -10,18 +10,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
 namespace BL
 {
-   public class FindParkingBl
+    public class FindParkingBl
     {
-        List<ParkingEntities> parkingList = new List<ParkingEntities>(); // רשימה של החניות
-        List<ParkingEntities> freeParkingList = new List<ParkingEntities>(); // רשימה של החניות הפנויות
-        List<RentEntities> Rental_list; //רשימת השכרות
-        List<double> distanceList = new List<double>(); //רשימה של כל המרחקים בין המיקום הנוכחי לבין רשימת החניות הפנויות                                                  
-        List<ParkingEntities> closestParkings = new List<ParkingEntities>(); //רשימה שתכיל את החניות הקרובות
+        public static List<ParkingEntities> parkingList = new List<ParkingEntities>(); // רשימה של החניות
+        public static List<ParkingEntities> freeParkingList = new List<ParkingEntities>(); // רשימה של החניות הפנויות
+        public static List<RentEntities> Rental_list; //רשימת השכרות
+        public static List<double> distanceList = new List<double>(); //רשימה של כל המרחקים בין המיקום הנוכחי לבין רשימת החניות הפנויות                                                  
+        public static List<ParkingEntities> closestParkings = new List<ParkingEntities>(); //רשימה שתכיל את החניות הקרובות
 
         //מחזירה רשימה של שלוש חניות קרובות
-        public List<ParkingEntities> start(double home_lat, double home_lan, RentEntities rent)
+        public static List<ParkingEntities> start(double home_lat, double home_lan, RentEntities rent)
         {
             parkingList = ParkingBl.GetParkingList();//שליפת כל החניות
 
@@ -42,17 +44,17 @@ namespace BL
             //סינון רשימת החניות הפנויות ורשימת המרחקים כך שהם יכילו רק מרחקים שזמן הנסיעה שלהם קטן מ-5 
             for (int i = 0; i < distanceList.Count; i++)
             {
-                if(distanceList[i] > 5)
+                if (distanceList[i] > 5)
                 {
-                    distanceList.Remove(distanceList[i]); 
-                    freeParkingList.Remove(freeParkingList[i]);    
+                    distanceList.Remove(distanceList[i]);
+                    freeParkingList.Remove(freeParkingList[i]);
                 }
             }
 
             //במקרה ורשימת החניות הפנויות לאחר הסינון קטן מ-3 נציג פחות חניות
             int numDidplay = 3; //ברירת המחדל תהיה הצגת 3 חניות
             //כל עוד רשימת החניות הפנויות קטנה ממספר החניות שנרצה להציג- נקטין את מספר החניות שנרצה להציג
-            while (freeParkingList.Count < numDidplay) 
+            while (freeParkingList.Count < numDidplay)
                 numDidplay--;
 
             //במקרה ולא קיים חניות פנויות להצגה, נחזיר כבר מעכשיו רשימת חניות ריקה
@@ -60,7 +62,7 @@ namespace BL
                 return closestParkings;
 
             //רשימה שתכיל את האינדקסים של המרחקים הקטנים ביותר
-            List<int> MinDistanceIndex=new List<int>();
+            List<int> MinDistanceIndex = new List<int>();
             double minDistance; //משתנה שיכיל את המרחק המינימלי הנוכחי
             int indexOfMin; //משתנה שיכיל את האינדקס של המרחק המינימלי הנוכחי
 
@@ -83,7 +85,7 @@ namespace BL
 
         //פונקציה המקבלת קוד חנייה ובודקת האם החניה פנויה להשכרה בשעה והתאריך הרצויים
         //במקרה שהחניה פנויה הפונקציה מוסיפה את החניה לרשימת החניות הפנויות
-        public void freeParking(int idParking, DateTime entryDate, DateTime leavingDate, DateTime entryHour, DateTime leavingHour)
+        public static void freeParking(int idParking, DateTime entryDate, DateTime leavingDate, DateTime entryHour, DateTime leavingHour)
         {
             bool free = true; //בתחילה נגדיר שהחנייה פנויה
             Rental_list = RentBl.GetRentList();  //נכניס לרשימת ההשכרות את כל ההשכרות הקיימות
@@ -117,12 +119,15 @@ namespace BL
 
 
         //מקבלת 2 כתובות: כתובת יעד וכתובת מקור ומחזירה מרחק לפי זמן נסיעה
-        public double GetDuration(double lat_1, double lng_1, double lat_2, double lng_2)
+        public static double GetDuration(double lat_1, double lng_1, double lat_2, double lng_2)
         {
             DirectionsRequest request = new DirectionsRequest();
-            request.Key = "AIzaSyALloXnE20WhqF0szj5Ak2C6074nQQk9uE";//key of google maps
-            //request.Origin = new Location(lat_1, lng_1);
-            //request.Destination = new Location(lat_2, lng_2);
+            request.Key = "AIzaSyC6wEMLBoWos4fb1Vgzmukt25AzX_6ogcw";//key of google maps
+            // request.Key = "AIzaSyALloXnE20WhqF0szj5Ak2C6074nQQk9uE";//key of google maps
+            request.Origin = new Location(lat_1, lng_1);
+            request.Destination = new Location(lat_2, lng_2);
+            
+            
             var response = GoogleApi.GoogleMaps.Directions.Query(request);
             double duration;
             if (response.Routes.Count() > 0)
